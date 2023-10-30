9c474cfa22926302b503319b9f14e9c1e0b779ea
Ying Lu <yinglu5374@gmail.com>
2023-10-30 19:03:50 -0400

    update

--- src/lib/movieApi.ts
+++ src/lib/movieApi.ts
@@ -1,4 +1,4 @@
-const API_URL = `http://www.omdbapi.com?apikey=${process.env.MOVIE_API_KEY}`
+const API_URL = `http://www.omdbapi.com?apikey=b6003d8a`
 
 type ResponseType = {
     Search: MovieType[]
@@ -8,7 +8,7 @@ export const getMovies = async (title: string): Promise<ResponseType> => {
   const url = `${API_URL}&s=${title}`
   
   console.log(`url=${url}`)
-  // url=http://www.omdbapi.com?apikey=xxxx&s=war
+  // url=http://www.omdbapi.com?apikey=b6003d8a&s=war
 
   const response = await fetch(url)
 
